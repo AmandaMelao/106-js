b7a0e997737554b190bbdc204c827e02da148820
Amanda M Medeiros <amanda.melao.medeiros@gmail.com>
2019-06-10 16:41:03 -0300

    Criar algoritmo para Empréstimo e Encontrar Carta

---
+++ script.js
@@ -0,0 +1,35 @@
+let idade= number(prompt ("Digite sua idade: "));
+let salario; 
+let emprSolicitado;
+
+if (idade>= 25 && idade<=55){
+    (salario = number(prompt("Informe sua renda: "));
+        if(salario>= 1000){
+            (emprSolicitado = number(prompt("Informe o valor do empréstimo desejado"));
+                if(emprSolicitado <= 5*salario){
+                    alert("Empréstimo aceito");
+                }
+                else{
+                    alert("Empréstimo recusado devido ao valor solicitado ser alto");
+                }
+        else{
+            alert("Empréstimo recusado devido ao salário ser baixo");
+        }
+    else{
+        alert("Empréstimo indisponível pela idade);
+        }
+    }
+}
+
+let cor;
+let desenho;
+
+let desenhoP = (prompt("Digite o desenho ou valor da carta que procura"));
+let corP = (prompt("Digite a cor ou nipe da carta que procura"));
+
+if( desenhoP = desenho && corP = cor){
+    alert("Sua carta foi encontrada no baralho");
+}
+else{
+    alert("Sua carta não se encontra no baralho");
+}
